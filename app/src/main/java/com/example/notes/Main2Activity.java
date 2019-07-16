ab99c1841752c56023c0affec133cb5cfad890c7
sayusharma <sayush68@gmail.com>
2019-07-17 00:11:11 +0530

    This is a Notes app.

---
+++ app/src/main/java/com/example/notes/Main2Activity.java
@@ -0,0 +1,53 @@
+package com.example.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
+
+import java.util.HashSet;
+
+public class Main2Activity extends AppCompatActivity {
+    int itemId;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        Intent intent = getIntent();
+        itemId = intent.getIntExtra("itemId",-1);
+        if(itemId!=-1){
+            Log.i("Editetxt",""+String.valueOf(editText));
+            editText.setText(MainActivity.arrayList.get(itemId));
+        }else{
+            MainActivity.arrayList.add("");
+            itemId = MainActivity.arrayList.size()-1;
+        }
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                MainActivity.arrayList.set(itemId,String.valueOf(charSequence));
+                MainActivity.arrayAdapter.notifyDataSetChanged();
+                SharedPreferences sharedPreferences = getSharedPreferences("com.example.notes",CONTEXT_RESTRICTED);
+                HashSet<String> set = new HashSet<>(MainActivity.arrayList);
+                sharedPreferences.edit().putStringSet("notes",set).apply();
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.notes;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class MainActivity extends AppCompatActivity {
+    static ArrayAdapter<String> arrayAdapter;
+    static ArrayList<String> arrayList=new ArrayList<String>();
+    SharedPreferences sharedPreferences;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater =getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if(item.getItemId() == R.id.addNote){
+            Intent intent = new Intent(getApplicationContext(),Main2Activity.class);
+            startActivity(intent);
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public void addNote(int i)
+    {
+        Intent intent = new Intent(getApplicationContext(),Main2Activity.class);
+        intent.putExtra("itemId",i);
+        startActivity(intent);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        sharedPreferences = getSharedPreferences("com.example.notes",CONTEXT_RESTRICTED);
+        HashSet<String> set = (HashSet<String>) sharedPreferences.getStringSet("notes",null);
+        if(set == null)
+            arrayList.add("Welcome note!");
+        else{
+            arrayList=new ArrayList<String>(set);
+        }
+        arrayAdapter =new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                addNote(i);
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final int itemToDelete = i;
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Do you want to delete this note?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                arrayList.remove(itemToDelete);
+                                arrayAdapter.notifyDataSetChanged();
+                                HashSet<String> set = new HashSet<>(MainActivity.arrayList);
+                                sharedPreferences.edit().putStringSet("notes",set).apply();
+                            }
+                        })
+                        .setNegativeButton("No",null)
+                        .show();
+                if(itemToDelete!=-1){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+
+        });
+
+    }
+}
